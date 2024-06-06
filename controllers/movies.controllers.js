958b0a6f454a49755ae65930c45c289c8ee45bab
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-06-06 10:21:30 +0530

    added controller files

---
+++ controllers/movies.controllers.js
@@ -0,0 +1,15 @@
+export const movieIndex = (req, res) => {
+  res.send("This is get Method");
+};
+
+export const movieCreate = (req, res) => {
+  res.send("This is post Method");
+};
+
+export const movieUpdate = (req, res) => {
+  res.send("This is put method");
+};
+
+export const movieDelete = (req, res) => {
+  res.send("this is delete method");
+};
